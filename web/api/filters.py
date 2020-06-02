844fa70b5e7fc1fe786357aa4511e8a7683a3e51
max <max@conquers.co>
2020-06-02 18:07:12 +0800

    fix: 會員管理列表，進階篩選結果跑不出來，篩選條件會被清空
    fix #21

--- web/api/filters.py
+++ web/api/filters.py
@@ -3,6 +3,7 @@ from rest_framework import filters
 from rest_framework.compat import coreapi, coreschema
 from django.utils import timezone
 from django.utils.timezone import make_aware
+import datetime
 
 or_q = lambda q, other_fn: other_fn if q is None else q | other_fn
 and_q = lambda q, other_fn: other_fn if q is None else q & other_fn
